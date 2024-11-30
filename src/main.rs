c8f9dab7dfb8bccadaa48577f6c3b9275e15cfc8
ryuji-1to <ryuji0222ss@icloud.com>
2024-11-30 16:54:16 +0900

    🎨 update

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
+mod handlers;
+mod repositories;
+
+use crate::repositories::{TodoRepository, TodoRepositoryForMemory};
 use axum::{
-    http::StatusCode,
-    response::IntoResponse,
+    extract::Extension,
     routing::{get, post},
-    Json, Router,
+    Router,
 };
-use serde::{Deserialize, Serialize};
-use std::env;
+use handlers::create_todo;
 use std::net::SocketAddr;
+use std::{env, sync::Arc};
 
 // Q: tokioってなんだっけ？
+// A: 非同期ランタイム
 #[tokio::main]
 async fn main() {
+    // logging
     let log_level = env::var("RUST_LOG").unwrap_or("info".to_string());
     env::set_var("RUST_LOG", log_level);
     // tracingの初期化をする
     tracing_subscriber::fmt::init();
+    let repository = TodoRepositoryForMemory::new();
     // アプリケーションのルーティング設定を作成
-    let app = create_app();
+    let app = create_app(repository);
     // std::convert::Fromトレイとを実装しているため、fromメソッドが使える
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {}", addr);
@@ -27,78 +33,33 @@ async fn main() {
         .unwrap();
 }
 
-fn create_app() -> Router {
+fn create_app<T: TodoRepository>(repository: T) -> Router {
     Router::new()
         .route("/", get(root))
-        .route("/users", post(create_user))
+        .route("/todos", post(create_todo::<T>))
+        .layer(Extension(Arc::new(repository)))
 }
 
 async fn root() -> &'static str {
     "Hello, World!"
 }
 
-async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
-    // Deserialize
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
-    // Serialize
-    (StatusCode::CREATED, Json(user))
-}
-
-// Q: deriveってなんだっけ
-// A: トレイトを付与する
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
-struct CreateUser {
-    username: String,
-}
-
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
-struct User {
-    id: u64,
-    username: String,
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
-    use axum::{
-        body::Body,
-        http::{header, Method, Request},
-    };
+    use axum::{body::Body, http::Request};
     use tower::ServiceExt;
 
     #[tokio::test]
     async fn should_return_hello_world() {
+        let repository = TodoRepositoryForMemory::new();
         // リクエストの生成
         let req = Request::builder().uri("/").body(Body::empty()).unwrap();
         // oneshotは非同期関数なのでawaitする、必ずOkが戻り値となるのでunwrapする
-        let res = create_app().oneshot(req).await.unwrap();
+        let res = create_app(repository).oneshot(req).await.unwrap();
         // 得られたresponseはそのままだと扱えないので、hyperを使ってBytes型を経てString型に変異案
         let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
         let body: String = String::from_utf8(bytes.to_vec()).unwrap();
         assert_eq!(body, "Hello, World!");
-    }
-
-    #[tokio::test]
-    async fn should_return_user_data() {
-        let req = Request::builder()
-            .uri("/users")
-            .method(Method::POST)
-            .header(header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
-            .body(Body::from(r#"{"username": "田中 太郎"}"#))
-            .unwrap();
-        let res = create_app().oneshot(req).await.unwrap();
-        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
-        let body: String = String::from_utf8(bytes.to_vec()).unwrap();
-        let user: User = serde_json::from_str(&body).expect("cannot convert User instance.");
-        assert_eq!(
-            user,
-            User {
-                id: 1337,
-                username: "田中 太郎".to_string()
-            }
-        )
     }
 }
